259cf105bb096c721aeb549aceb7ff91b2a1984b
Yury Lebedev <yurylebedev@mail.ru>
2018-03-15 00:27:43 +0300

    Словарь и выражения для Милены Nuance Vocalizer Expressive 3
    http://4pda.ru/forum/index.php?s=&showtopic=200728&view=findpost&p=71321447

--- src/build_dictionary_alerts.bat
+++ src/build_dictionary_alerts.bat
@@ -1,2 +1,2 @@
-"C:\Program Files (x86)\Python25\python.exe" "C:\Program Files (x86)\Nuance\vocon3200\EDS_v3_3F1\tools\dictcpl.pyc" --dictionaryFilepath=alerts.dct --outputFilepath=..\dist\milena_userdct_rur.dat
+"C:\Program Files (x86)\Python25\python.exe" "C:\Program Files (x86)\Nuance\vocon3200\EDS_v3_3F1\tools\dictcpl.pyc" --dictionaryFilepath=alerts.dct "--outputFilepath=..\dist\Vocalizer Expressive 3 Milena\userdct_rur_milena.dat"
 pause
