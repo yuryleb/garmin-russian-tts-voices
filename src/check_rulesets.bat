548518759a687ac8f0a40c4bdc9f1d45e981656a
Yury Lebedev <yurylebedev@mail.ru>
2018-04-18 14:51:30 +0300

    Обработка сложных названий НП и улиц в отдельном RP_RSET.TXT
    Причем добавление type=address в RP_RSET.TXT, кажется, удаляет этот тэг после прохода этим файлом и склонения и многое другое, завязанное на address в основном RULESET.TXT перестает работать

--- src/check_rulesets.bat
+++ src/check_rulesets.bat
@@ -1,3 +1,5 @@
 ..\test\RulesetChecker.exe "Pycckuu__Milena 2.10\RULESET.TXT"
+..\test\RulesetChecker.exe "Pycckuu__Milena 2.10\RP_RSET.TXT"
 ..\test\RulesetChecker.exe "Pycckuu__Tatiana 2.30\RULESET.TXT"
+..\test\RulesetChecker.exe "Pycckuu__Tatiana 2.30\RP_RSET.TXT"
 pause
---
+++ test/TestRP_RSET.bat
@@ -0,0 +1,4 @@
+chcp 65001
+RulesetChecker.exe "..\src\Pycckuu__Milena 2.10\RP_RSET.TXT" TestRP_RSET.txt 2>TestRP_RSET_Milena.log
+RulesetChecker.exe "..\src\Pycckuu__Tatiana 2.30\RP_RSET.TXT" TestRP_RSET.txt 2>TestRP_RSET_Tatiana.log
+pause
