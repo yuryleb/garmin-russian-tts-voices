549c24c97a96b9c0e2ea6cfbf845f5bf665804dd
Yury Lebedev <yurylebedev@mail.ru>
2018-05-04 09:22:51 +0300

    RP_RSET.TXT выполняется перед RULESET.TXT
    Первоначальное предположение о выполнении "главного" файла регулярных выражений RULESET.TXT всегда первым было ошибочным

--- test/TestRULESET.bat
+++ test/TestRULESET.bat
@@ -1,4 +1,4 @@
 chcp 65001
-RulesetChecker.exe "..\src\Pycckuu__Milena 2.10\RULESET.TXT" "..\src\Pycckuu__Milena 2.10\RP_RSET.TXT" TestRULESET.txt 2>TestRULESET_Milena.log
-RulesetChecker.exe "..\src\Pycckuu__Tatiana 2.30\RULESET.TXT" "..\src\Pycckuu__Tatiana 2.30\RP_RSET.TXT" TestRULESET.txt 2>TestRULESET_Tatiana.log
+RulesetChecker.exe "..\src\Pycckuu__Milena 2.10\RP_RSET.TXT" "..\src\Pycckuu__Milena 2.10\RULESET.TXT" TestRULESET.txt 2>TestRULESET_Milena.log
+RulesetChecker.exe "..\src\Pycckuu__Tatiana 2.30\RP_RSET.TXT" "..\src\Pycckuu__Tatiana 2.30\RULESET.TXT" TestRULESET.txt 2>TestRULESET_Tatiana.log
 pause
